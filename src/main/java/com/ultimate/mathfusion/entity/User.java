b4dc19f31f62234325c977f6ec6e64933b8b4ed1
SJY <bangstation@naver.com>
2025-03-24 11:20:09 +0900

    ERD_chore & user entity_데이터베이스의 테이블명을 명시 및 연결

--- src/main/java/com/ultimate/mathfusion/entity/User.java
+++ src/main/java/com/ultimate/mathfusion/entity/User.java
@@ -12,7 +12,7 @@ import org.springframework.security.core.userdetails.UserDetails;
 import java.util.Collection;
 import java.util.List;
 
-@Table(name = "users")
+@Table(name = "user")
 @NoArgsConstructor(access = AccessLevel.PROTECTED)
 @Getter
 @Entity
